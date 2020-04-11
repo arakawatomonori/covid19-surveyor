241f00e1ce9a24bf3e878440f3d1b1f249aee4f6
yuiseki <yuiseki@gmail.com>
2020-04-11 10:11:03 +0000

    robots.txtを全ディレクトリに設置する

--- wget.sh
+++ wget.sh
@@ -1,122 +1,198 @@
 
 # 首相官邸
 wget -l 3 -r --no-check-certificate http://www.kantei.go.jp/
+cp robots.txt www.kantei.go.jp
 # 内閣府
 wget -l 3 -r --no-check-certificate https://www.cao.go.jp/
+cp robots.txt www.cao.go.jp
 # 内閣府防災情報
 wget -l 3 -r --no-check-certificate https://www.bousai.go.jp/
+cp robots.txt www.bousai.go.jp
 # 復興庁
 wget -l 3 -r --no-check-certificate https://www.reconstruction.go.jp/
+cp robots.txt www.reconstruction.go.jp
 # 公正取引委員会
 wget -l 3 -r --no-check-certificate https://www.jftc.go.jp/
+cp robots.txt www.jftc.go.jp
 # 日本学術会議
 wget -l 3 -r --no-check-certificate http://www.scj.go.jp/
+cp robots.txt www.scj.go.jp
 # 警察庁
 wget -l 3 -r --no-check-certificate http://www.npa.go.jp/
+cp robots.txt www.npa.go.jp
 # 金融庁
 wget -l 3 -r --no-check-certificate https://www.fsa.go.jp/
+cp robots.txt www.fsa.go.jp
 # 消費者庁
 wget -l 3 -r --no-check-certificate https://www.caa.go.jp/
+cp robots.txt www.caa.go.jp
 # 総務省
 wget -l 3 -r --no-check-certificate http://www.soumu.go.jp/
+cp robots.txt www.soumu.go.jp
 # 総務省消防庁
 wget -l 3 -r --no-check-certificate https://www.fdma.go.jp/
+cp robots.txt www.fdma.go.jp
 # 法務省
 wget -l 3 -r --no-check-certificate https://www.moj.go.jp/
+cp robots.txt www.moj.go.jp
 # 外務省
 wget -l 3 -r --no-check-certificate https://www.mofa.go.jp/
+cp robots.txt www.mofa.go.jp
 # 財務省
 wget -l 3 -r --no-check-certificate https://www.mof.go.jp/
+cp robots.txt www.mof.go.jp
 # 財務省 国税庁
 wget -l 3 -r --no-check-certificate https://www.nta.go.jp/
+cp robots.txt www.nta.go.jp
 # 文部科学省
 wget -l 3 -r --no-check-certificate https://www.mext.go.jp/
+cp robots.txt www.mext.go.jp
 # 文部科学省 文化庁
 wget -l 3 -r --no-check-certificate https://www.bunka.go.jp/
+cp robots.txt www.bunka.go.jp
 # 厚生労働省
 wget -l 3 -r --no-check-certificate https://www.mhlw.go.jp/
+cp robots.txt www.mhlw.go.jp
 # 農林水産省
 wget -l 3 -r --no-check-certificate https://www.maff.go.jp/
+cp robots.txt www.maff.go.jp
 # 経済産業省
 wget -l 3 -r --no-check-certificate https://www.meti.go.jp/
+cp robots.txt www.meti.go.jp
 # 経済産業省 特許庁
 wget -l 3 -r --no-check-certificate https://www.jpo.go.jp/
+cp robots.txt www.jpo.go.jp
 # 経済産業省 中小企業庁
 wget -l 3 -r --no-check-certificate https://www.chusho.meti.go.jp/
+cp robots.txt www.chuso.meti.go.jp
 # 国土交通省
 wget -l 3 -r --no-check-certificate https://www.mlit.go.jp/
+cp robots.txt www.mlit.go.jp
 # 国土交通省 国土地理院
 wget -l 3 -r --no-check-certificate https://www.gsi.go.jp/
+cp robots.txt www.gsi.go.jp
 # 国土交通省 気象庁
 wget -l 3 -r --no-check-certificate https://www.jma.go.jp/
+cp robots.txt www.jma.go.jp
 # 国土交通省 海上保安庁
 wget -l 3 -r --no-check-certificate https://www.kaiho.milt.go.jp/
+cp robots.txt www.kaiiho.milt.go.jp
 # 環境省
 wget -l 3 -r --no-check-certificate https://www.env.go.jp/
+cp robots.txt www.env.go.jp
 # 環境省 原子力規制委員会
 wget -l 3 -r --no-check-certificate https://www.nsr.go.jp/
+cp robots.txt www.nsr.go.jp
 # 防衛省
 wget -l 3 -r --no-check-certificate https://www.mod.go.jp/
+cp robots.txt www.mod.go.jp
 
 # 独立行政法人
 # 中小企業基盤整備機構
 wget -l 3 -r --no-check-certificate https://j-net21.smrj.go.jp/
+cp robots.txt j-net21.smrj.go.jp
 
 
 
 
 # 北海道
 wget -l 3 -r --no-check-certificate http://www.pref.hokkaido.lg.jp/
+cp robots.txt www.pref.hokkaido.lg.jp
 # 東北
 wget -l 3 -r --no-check-certificate http://www.pref.aomori.lg.jp/
+cp robots.txt www.pref.aomori.lg.jp
 wget -l 3 -r --no-check-certificate http://www.pref.iwate.jp/
+cp robots.txt www.pref.iwate.jp
 wget -l 3 -r --no-check-certificate http://www.pref.miyagi.jp/
+cp robots.txt www.pref.miyagi.jp
 wget -l 3 -r --no-check-certificate http://www.pref.akita.lg.jp/
+cp robots.txt www.pref.akita.lg.jp
 wget -l 3 -r --no-check-certificate http://www.pref.yamagata.jp/
+cp robots.txt www.pref.yamagata.jp
 wget -l 3 -r --no-check-certificate http://www.pref.fukushima.lg.jp/
+cp robots.txt www.pref.fukushima.lg.jp
 # 関東
 wget -l 3 -r --no-check-certificate http://www.pref.ibaraki.jp/
+cp robots.txt www.pref.ibaraki.jp
 wget -l 3 -r --no-check-certificate http://www.pref.tochigi.lg.jp/
+cp robots.txt www.pref.tochigi.lg.jp
 wget -l 3 -r --no-check-certificate http://www.pref.gunma.jp/
+cp robots.txt www.pref.gunma.jp
 wget -l 3 -r --no-check-certificate http://www.pref.saitama.lg.jp/
+cp robots.txt www.pref.saitama.lg.jp
 wget -l 3 -r --no-check-certificate http://www.metro.tokyo.jp/
+cp robots.txt www.metro.tokyo.jp
 wget -l 3 -r --no-check-certificate http://www.pref.kanagawa.jp/
+cp robots.txt www.pref.kanagawa.jp
 # 中部
 wget -l 3 -r --no-check-certificate http://www.pref.niigata.lg.jp/
+cp robots.txt www.pref.niigata.lg.jp
 wget -l 3 -r --no-check-certificate http://www.pref.toyama.jp/
+cp robots.txt www.pref.toyama.jp
 wget -l 3 -r --no-check-certificate http://www.pref.ishikawa.lg.jp/
+cp robots.txt www.pref.ishikawa.lg.jp
 wget -l 3 -r --no-check-certificate http://www.pref.fukui.lg.jp/
+cp robots.txt www.pref.fukui.lg.jp
 wget -l 3 -r --no-check-certificate http://www.pref.yamanishi.jp/
+cp robots.txt www.pref.yamanashi.jp
 wget -l 3 -r --no-check-certificate http://www.pref.nagano.lg.jp/
+cp robots.txt www.pref.nagano.lg.jp
 wget -l 3 -r --no-check-certificate http://www.pref.gifu.lg.jp/
+cp robots.txt www.pref.gifu.lg.jp
 wget -l 3 -r --no-check-certificate http://www.pref.shizuoka.jp/
+cp robots.txt www.pref.shizuoka.jp
 wget -l 3 -r --no-check-certificate http://www.pref.aichi.jp/
+cp robots.txt www.pref.aichi.jp
 # 近畿
 wget -l 3 -r --no-check-certificate http://www.pref.mie.lg.jp/
+cp robots.txt www.pref.mie.lg.jp
 wget -l 3 -r --no-check-certificate http://www.pref.shiga.lg.jp/
+cp robots.txt www.pref.shiga.lg.jp
 wget -l 3 -r --no-check-certificate http://www.pref.kyoto.jp/
+cp robots.txt www.pref.kyoto.jp
 wget -l 3 -r --no-check-certificate http://www.pref.osaka.lg.jp/
+cp robots.txt www.pref.osaka.lg.jp
 wget -l 3 -r --no-check-certificate http://web.pref.hyogo.lg.jp/
+cp robots.txt web.pref.hyogo.lg.jp
 wget -l 3 -r --no-check-certificate http://www.pref.nara.jp/
+cp robots.txt www.pref.nara.jp
 wget -l 3 -r --no-check-certificate http://www.pref.wakayama.lg.jp/
+cp robots.txt www.pref.wakayama.lg.jp
 # 中国
 wget -l 3 -r --no-check-certificate http://www.pref.tottori.lg.jp/
+cp robots.txt www.pref.tottori.lg.jp
 wget -l 3 -r --no-check-certificate http://www.pref.shimane.lg.jp/
+cp robots.txt www.pref.shimane.lg.jp
 wget -l 3 -r --no-check-certificate http://www.pref.okayama.lg.jp/
+cp robots.txt www.pref.okayama.lg.jp
 wget -l 3 -r --no-check-certificate http://www.pref.hiroshima.lg.jp/
+cp robots.txt www.pref.hiroshima.lg.jp
 wget -l 3 -r --no-check-certificate http://www.pref.yamaguchi.lg.jp/
+cp robots.txt www.pref.yamaguchi.lg.jp
 # 四国
 wget -l 3 -r --no-check-certificate http://www.pref.kagawa.lg.jp/
+cp robots.txt www.pref.kagawa.lg.jp
 wget -l 3 -r --no-check-certificate http://www.pref.ehime.jp/
+cp robots.txt www.pref.ehime.jp
 wget -l 3 -r --no-check-certificate http://www.pref.kochi.lg.jp/
+cp robots.txt www.pref.kochi.lg.jp
 wget -l 3 -r --no-check-certificate http://www.pref.tokushima.lg.jp/
+cp robots.txt www.pref.tokushima.lg.jp
 # 九州
 wget -l 3 -r --no-check-certificate http://www.pref.fukuoka.lg.jp/
+cp robots.txt www.pref.fukuoka.lg.jp
 wget -l 3 -r --no-check-certificate http://www.pref.saga.lg.jp/
+cp robots.txt www.pref.saga.lg.jp
 wget -l 3 -r --no-check-certificate http://www.pref.nagasaki.lg.jp/
+cp robots.txt www.pref.nagasaki.lg.jp
 wget -l 3 -r --no-check-certificate http://www.pref.kumamoto.jp/
+cp robots.txt www.pref.kumamoto.jp
 wget -l 3 -r --no-check-certificate http://www.pref.oita.jp/
+cp robots.txt www.pref.oita.jp
 wget -l 3 -r --no-check-certificate http://www.pref.miyazaki.lg.jp/
+cp robots.txt www.pref.miyazaki.lg.jp
 wget -l 3 -r --no-check-certificate http://www.pref.kagoshima.jp/
+cp robots.txt www.pref.kagoshima.jp
 # 沖縄
 wget -l 3 -r --no-check-certificate http://www.pref.okinawa.jp/
+cp robots.txt www.pref.okinawa.jp
