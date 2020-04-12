a628d263a50cccd7a7482d2f205b560ab3051709
yuiseki <yuiseki@gmail.com>
2020-04-12 02:11:42 +0000

    インクリメンタル検索実装

--- index.sh
+++ index.sh
@@ -2,7 +2,7 @@ files="./*"
 filearray=()
 dirarray=()
 for path in $files; do
-	if [[ -f $path && $path =~ ^\.\/grep_.*$ ]] ; then
+	if [[ -f $path && $path =~ ^\.\/.*\.txt*$ ]] ; then
 		filearray+=("$path")
 	fi
 	if [ -d $path ] ; then
@@ -10,11 +10,52 @@ for path in $files; do
 	fi
 done
 
-for i in ${filearray[@]}; do
-	filepath=$(cut -d'/' -f 2 <<< $i)
-	link="<a href='http://gov.yuiseki.net/${filepath}'>${filepath}</a><br />"
-	echo $link
-done
+
+js=`cat <<EOM
+<script type="text/javascript">
+function quotemeta (string) {
+	return string.replace(/(\W)/, "\\$1");
+}
+function isearch (pattern) {
+	var regex = new RegExp(quotemeta(pattern), "i");
+	var spans = document.getElementsByTagName('span');
+	var length = spans.length;
+	for (var i = 0; i < length; i++) {
+		var e = spans[i];
+		if (e.className == "line") {
+			if (pattern !== "" && e.innerHTML.match(regex)) {
+				e.style.display = "block";
+			} else {
+				e.style.display = "none";
+			}
+		}
+	}
+}
+</script>
+EOM
+`
+echo $js
+
+form=`cat <<EOM
+<form onsubmit="return false;">
+	<input type="text" name="pattern" onkeyup="isearch(this.value)">
+</form>
+EOM
+`
+echo $form
+
+
+echo "<div>"
+while read line; do
+	url=$(cut -d':' -f 1 <<< $line)
+	text=$(cut -d':' -f 2 <<< $line)
+	echo "<span class='line' style='display: none;'>"
+	echo "<a href='${url}'>"
+	echo $text
+	echo "</a>"
+	echo "</span>"
+done < uniq.txt
+echo "</div>"
 
 echo "<hr />"
 
---
+++ result.sh
@@ -0,0 +1,4 @@
+
+cat grep*.txt > cat.txt
+sort cat.txt > sort.txt
+uniq -d sort.txt > uniq.txt
