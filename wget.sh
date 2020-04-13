555536940990455885f1b55bca3848d5bfee1c2f
TAKANO Mitsuhiro <takano32@gmail.com>
2020-04-13 19:40:28 +0900

    donalod site data to www-data directory

--- wget.sh
+++ wget.sh
@@ -1,11 +1,15 @@
 
+#!/bun/bash
+
 urls=`cat data/gov.csv data/pref.csv`
 domains=${urls//http:\/\//}
 domains=${domains//https:\/\//}
 
+cd www-data
 echo $urls | xargs -n 1 echo | xargs -P 0 -I{} wget -l 2 -r --no-check-certificate {}
 echo $domains | xargs -n 1 echo | xargs -I{} cp -f robots.txt {}
-
+cd -
 
 ./grep.sh
 ./index.sh > index.html
+
