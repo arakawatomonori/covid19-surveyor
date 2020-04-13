741f8ac798ff678aade4ba2ed2ad12e4667243a8
yuiseki <yuiseki@gmail.com>
2020-04-13 10:46:23 +0000

    parse CSV

--- wget.sh
+++ wget.sh
@@ -1,7 +1,17 @@
+files=("data/gov.csv data/pref.csv")
+echo $files
+urls=()
+domains=()
 
-urls=`cat data/gov.csv data/pref.csv`
-domains=${urls//http:\/\//}
-domains=${domains//https:\/\//}
+for file in $files; do
+	for line in `cat ${file}`; do
+		url=`echo ${line} | cut -d',' -f 3`
+		urls=("${urls} $url")
+		domain=${url//http:\/\//}
+		domain=${domain//https:\/\//}
+		domains=("${domains} $domain")
+	done
+done
 
 echo $urls | xargs -n 1 echo | xargs -P 0 -I{} wget -l 2 -r --no-check-certificate {}
 echo $domains | xargs -n 1 echo | xargs -I{} cp -f robots.txt {}
