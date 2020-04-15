ffc98b414f86af664eff2d7bd28622a5a32e76a1
yuiseki <yuiseki@gmail.com>
2020-04-15 12:52:21 +0900

    Update aggregate.sh

--- aggregate.sh
+++ aggregate.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e
 
+###
+### About
+### grep.shで./grep-dataに収集した情報を結合し、ソートし、重複を取り除くスクリプト
+###
+### Usage
+### ./aggregate.sh
+###
+
+
 cat ./grep-data/grep_コロナ_*.txt.tmp > ./tmp/cat.txt.tmp
 sort ./tmp/cat.txt.tmp > ./tmp/sort.txt.tmp
 uniq -d ./tmp/sort.txt.tmp > result.txt
