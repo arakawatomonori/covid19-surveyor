83b53f9eca9fb362af44dd4a5aa2b62b7dc12367
yuiseki <yuiseki@gmail.com>
2020-04-15 03:39:30 +0000

    make test追加

--- Makefile
+++ Makefile
@@ -1,14 +1,19 @@
-.PHONY: all wget grep aggregate index
+.PHONY: test all wget grep aggregate index
 
+test:
+	./wget.sh data/test.csv
+	./grep.sh
+	./aggregate.sh
+	./index.sh > ./www-data/index.html
 
 all:
-	./wget.sh
+	./wget.sh data/gov.csv data/pref.csv data/city.csv
 	./grep.sh
 	./aggregate.sh
 	./index.sh > ./www-data/index.html
 
 wget:
-	./wget.sh
+	./wget.sh data/gov.csv data/pref.csv data/city.csv
 
 grep:
 	./grep.sh
--- wget.sh
+++ wget.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 set -e
 
-files=("data/gov.csv data/pref.csv data/city.csv")
+#files=("data/gov.csv data/pref.csv data/city.csv")
 
 urls=()
 domains=()
-
-for file in $files; do
-	for line in `cat ${file}`; do
+while (( $# > 0 ))
+do
+	for line in `cat $1`; do
 		url=`echo ${line} | cut -d',' -f 3`
 		urls=("${urls} $url")
 		domain=${url//http:\/\//}
 		domain=${domain//https:\/\//}
 		domains=("${domains} $domain")
 	done
+	shift
 done
+
+
 
 cd www-data
 echo $urls | xargs -n 1 echo | xargs -P 16 -I{} wget -l 2 -r --no-check-certificate {}
