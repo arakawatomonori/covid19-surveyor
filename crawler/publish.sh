995e886c13dac1852d4959d397d94d4e4fd92093
Ubuntu <ubuntu@vscovid-crawler.c.vscovid19.internal>
2020-04-16 05:57:18 +0000

    update publish.sh

--- crawler/publish.sh
+++ crawler/publish.sh
@@ -10,31 +10,27 @@ set -e
 ###
 
 
-cd www-data
-files="./*"
-dirarray=()
-for path in $files; do
-	if [ -d $path ] ; then
-		dirarray+=("$path")
-	fi
-done
-cd -
-
 js=`cat <<EOM
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>新型コロナウイルス各自治体の経済支援制度まとめ</title>
+<style type="text/css">
+<!--
+li.line {font-size:1.4em; margin:5px;}
+-->
+</style>
 <script type="text/javascript">
 function quotemeta (string) {
 	return string.replace(/(\W)/, "\\$1");
 }
 function isearch (pattern) {
 	var regex = new RegExp(quotemeta(pattern), "i");
-	var spans = document.getElementsByTagName('span');
+	var spans = document.getElementsByTagName('li');
 	var length = spans.length;
 	for (var i = 0; i < length; i++) {
 		var e = spans[i];
 		if (e.className == "line") {
 			if (e.innerHTML.match(regex)) {
-				e.style.display = "block";
+				e.style.display = "list-item";
 			} else {
 				e.style.display = "none";
 			}
@@ -47,6 +43,7 @@ EOM
 echo $js
 
 form=`cat <<EOM
+<h1>新型コロナウイルス各自治体の経済支援制度まとめ</h1>
 <form onsubmit="return false;">
 	<input type="text" name="pattern" placeholder="キーワードで絞り込み" onkeyup="isearch(this.value)" style="font-size:2em;">
 </form>
@@ -55,37 +52,30 @@ EOM
 echo $form
 
 
-echo "<div style='height:600px;overflow:scroll;'>"
+echo "<ul>"
 keys=`redis-cli KEYS "vscovid-crawler:result-*"`
 for key in $keys; do
         result=`redis-cli GET $key`
-        url=`echo $result| cut -d',' -f 1`
         bool=`echo $result| cut -d',' -f 4`
-				# ドメイン名から自治体名を得る
-				domain=$(cut -d'/' -f 3 <<< $url)
-				govname=`grep $domain --include="*.csv" ./data/*|cut -d',' -f 1|cut -d':' -f 2`
-				# urlから詳細を得る
-				path=${url//http:\/\//}
-				path=${path//https:\/\//}
-				title=`grep $path ./results.txt |cut -d':' -f 2`
-
         if [ $bool = "true" ]; then
-                echo "<span class='line' style='display: block;'>"
+		url=`echo $result| cut -d',' -f 1`
+		# ドメイン名から自治体名を得る
+		domain=$(cut -d'/' -f 3 <<< $url)
+		govname=`grep $domain --include="*.csv" ./data/*|cut -d',' -f 1|cut -d':' -f 2`
+		# urlから詳細を得る
+		path=${url//http:\/\//}
+		path=${path//https:\/\//}
+		title=`grep $path ./result.txt |cut -d':' -f 2`
+
+                echo "<li class='line' style='display: list-item;'>"
                 echo "<a href='${url}'>"
                 echo $govname
                 echo ": "
                 echo $title
                 echo "</a>"
-                echo "</span>"
+                echo "</li>"
         fi
 done
-echo "</div>"
+echo "</ul>"
 
 
-echo "<hr />"
-
-for i in ${dirarray[@]}; do
-	domain=$(cut -d'/' -f 2 <<< $i)
-	link="<a href='http://${domain}.help.stopcovid19.jp'>${domain}</a><br />"
-	echo $link
-done
