822a1b1724ba305b93313e609e10ea03b2696b09
kobake <pg.kobake@gmail.com>
2020-04-18 15:04:38 +0000

    インデント調整（実際には将来的に4スペースにするとの合意がありますが、今は他のシェルに合わせてタブにしておきます）

--- lib/helper.sh
+++ lib/helper.sh
@@ -2,20 +2,20 @@
 set -e
 
 echo_green(){
-  echo -e "\t\e[32m $1 \e[m"
-  return 0
+    echo -e "\t\e[32m $1 \e[m"
+    return 0
 }
 
 echo_red() {
-  echo -e "\t\e[31m $1 \e[m"
-  return 0
+    echo -e "\t\e[31m $1 \e[m"
+    return 0
 }
 
 echo_indent(){
-  echo -e "\t" $1 $2
-  return 0
+    echo -e "\t" $1 $2
+    return 0
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-	exit 0
+    exit 0
 fi
