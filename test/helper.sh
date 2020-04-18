de36276cac430c8d6a2fb6ce5330f65c343902ed
yuiseki <yuiseki@gmail.com>
2020-04-18 14:38:27 +0000

    よくわからん

--- test/helper.sh
+++ test/helper.sh
@@ -2,14 +2,17 @@
 
 echo_green(){
   echo -e "\t\e[32m $1 \e[m"
+  return 0
 }
 
 echo_red() {
   echo -e "\t\e[31m $1 \e[m"
+  return 0
 }
 
 echo_indent(){
   echo -e "\t" $1 $2
+  return 0
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
