7836c21e0b45b7bddedab59bc0be83f0b3512fdb
yuiseki <yuiseki@gmail.com>
2020-04-21 03:07:02 +0000

    テストも更新

--- test/grep_test.sh
+++ test/grep_test.sh
@@ -12,12 +12,6 @@ actual=$(echo $input | sanitize_grep_result)
 assert_equal "foo.html:あいうえおかきくけこさしすせそたちつてと" "$actual"
 
 echo test sanitize_grep_result
-## remove too long line
-input="foo.html:$(seq 100 | xargs)"
-actual=$(echo $input | sanitize_grep_result)
-assert_empty "$actual"
-
-echo test sanitize_grep_result
 ## sanitize HTML tags
 input="foo.html:<p><a href=\"bar.html\">テキストテキスト<br>テキスト</a></p>"
 actual=$(echo $input | sanitize_grep_result)
