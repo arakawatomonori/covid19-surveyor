16bc1effb38197c33f27e93df524824f57e12905
kobake <pg.kobake@gmail.com>
2020-04-22 09:06:02 +0000

    体裁整え：コメント入れ、インデント調整、lib 取込位置の調整、等

--- slack-bot/url-vote-rank.sh
+++ slack-bot/url-vote-rank.sh
@@ -15,6 +15,11 @@ set -e
 
 source .env
 
+# 依存lib
+. ./lib/slack-helper.sh
+
+
+# ユーザー毎の投票数を集計 -> tmp/rank.txt
 declare -A users
 
 keys=`redis-cli KEYS "vscovid-crawler-vote:count-*"`
@@ -28,19 +33,23 @@ for k in "${!users[@]}"; do
     echo $k,${users["$k"]}
 done | sort -t , -rn -k2 | head -n 20 > tmp/rank.txt
 
+
+# tmp/rank.txt を元にポスト用ランキング文字列を構築 -> $rank
 rank=""
 for line in `cat tmp/rank.txt`; do
     user_id=`echo $line | cut -d',' -f 1`
     count=`echo $line | cut -d',' -f 2`
     rank=$rank"\r\n<@$user_id> さん、 $count 回"
 done
-
 echo $rank
 
-. ./lib/slack-helper.sh
+
+# ポスト先チャンネル識別子
 channels_id=`get_channels_id`
 echo $channels_id
 
+
+# API へ送る用の JSON 構築
 json=`cat <<EOF
 {
     "channel": "${channels_id}",
@@ -65,9 +74,11 @@ json=`cat <<EOF
 EOF
 `
 
+
+# API ポスト実行
 echo "$json"
 wget -q -O - --post-data "$json" \
---header="Content-type: application/json" \
---header="Authorization: Bearer ${slack_token}" \
-https://slack.com/api/chat.postMessage | jq .
+    --header="Content-type: application/json" \
+    --header="Authorization: Bearer ${slack_token}" \
+    https://slack.com/api/chat.postMessage | jq .
 echo ""
