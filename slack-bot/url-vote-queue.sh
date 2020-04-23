c968bc0e76b65048950a865b96952d9031baab2b
yuiseki <yuiseki@gmail.com>
2020-04-23 10:25:10 +0000

    バグりまくっていた

--- slack-bot/url-vote-queue.sh
+++ slack-bot/url-vote-queue.sh
@@ -7,7 +7,7 @@ set -e
 source .env
 ts=`date '+%s'`
 
-namespace="redis-crawler-vote"
+namespace="vscovid-crawler-vote"
 
 keys=`redis-cli KEYS "vscovid-crawler:result-*"`
 for key in $keys; do
@@ -20,19 +20,23 @@ done
 # http://example.com/foo/bar.html のとき
 # domain_and_path=example.com/foo/bar.html
 for domain_and_path in `cat ./tmp/urls-uniq.txt`; do
+    # domain=example.com
+    domain=`echo $domain_and_path| cut -d'/' -f1 `
+    echo domain $domain
     # path=foo/bar.html
     path=`echo $domain_and_path | cut -d'/' -f 2-`
     echo path $path
-    domain=`echo $path| cut -d'/' -f1 `
-    # domain=example.com
-    echo domain $domain
-    top_url=`grep $domain --include="*.csv" ./data/*|cut -d',' -f 3`
     # top_url=https://example.com/index.html
+    top_url=`grep $domain --include="*.csv" ./data/*|cut -d',' -f 3`
+    if [ $top_url = "" ]; then
+	    continue
+    fi
     echo top_url $top_url
     # schema=https:
     schema=`echo $top_url | cut -d'/' -f 1`
     # url=https://example.com/foo/bar.html
     url="$schema//$domain/$path"
+    echo url $url
     md5=`get_md5_by_url $url`
     echo $md5
     # redisに存在しないことを確認する
