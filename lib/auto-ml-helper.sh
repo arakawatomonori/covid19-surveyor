9bb83529d6c9a3f1d8a1410cccfed365f24bb3ee
yuiseki <yuiseki@gmail.com>
2020-04-25 06:56:39 +0000

    add auto-ml-helper.sh

--- lib/auto-ml-helper.sh
+++ lib/auto-ml-helper.sh
@@ -7,9 +7,8 @@ gcp_token=$(gcloud auth application-default print-access-token)
 . ./lib/url-helper.sh
 . ./lib/string-helper.sh
 
-get_predict_by_url() {
-    url=$1
-    text=`get_text_by_url $url`
+build_predict_json() {
+    text=$1
     json=`cat <<EOF
     {
         "payload": {
@@ -21,13 +20,25 @@ get_predict_by_url() {
     }
 EOF
 `
+    echo $json
+}
 
+parse_predict_json() {
+    res=$1
+    echo $result
+}
+
+get_predict_by_url() {
+    url=$1
+    text=`get_text_by_url "$url"`
+    json=`build_predict_json "$text"`
+    echo $json > ./tmp/automl-req.json.log
     res=$(wget -q -O - --post-data "$json" \
     --header="Content-type: application/json" \
     --header="Authorization: Bearer ${gcp_token}" \
     https://automl.googleapis.com/v1/projects/11652160532/locations/us-central1/models/TCN2888562181702418432:predict)
-    echo "$res | jq payload[0].dysplayName, payload[0].classification.score"
-    
+    echo $res > ./tmp/automl-res.json.log
+    echo $res | jq -r '.payload[0] | [.displayName, .classification.score] | @csv'
 }
 
 # *.sh が直接実行されたら exit 0 する
