878717e6a216dc89e2557df2dfc02cae4a0c44c9
yuiseki <yuiseki@gmail.com>
2020-04-25 06:57:03 +0000

    update comment

--- test/auto-ml-helper_test.sh
+++ test/auto-ml-helper_test.sh
@@ -7,7 +7,7 @@ source .env
 . ./lib/auto-ml-helper.sh
 
 
-# environment=developmentの時だけ以下のテストを実行する
+# environment=developmentの時だけ以下のテストを実行するようにしたい
 
 echo test get_predict_by_url covid19_help
 actual=$(get_predict_by_url "https://www.pref.ehime.jp/h10500/kojinjigyo/kojin_kigenencho.html")
