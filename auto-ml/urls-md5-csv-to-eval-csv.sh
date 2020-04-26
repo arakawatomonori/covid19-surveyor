00273b579c34d11f5dd16455546043d660fb0469
yuiseki <yuiseki@gmail.com>
2020-04-26 09:01:51 +0000

    機械学習するために全URLのテキストを収集してcsvファイルにするスクリプト

---
+++ auto-ml/urls-md5-csv-to-eval-csv.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+
+# 依存lib
+. ./lib/url-helper.sh
+. ./lib/string-helper.sh
+
+get_text_by_url() {
+    url=$1
+    res=$(wget -q -O - --tries=1 --timeout=5 --dns-timeout=5 --connect-timeout=5 --read-timeout=5 $url)
+    if [ $? -ne 0 ]; then
+        return 1
+    fi
+    title=$(get_title_by_res "$res"|sed "s/\"/ /g")
+    desc=$(get_desc_by_res "$res" | remove_newline_and_comma | sed "s/\"/ /g")
+    echo "$title $desc"
+}
+
+
+echo "" > ./data/eval.csv
+while read line;do
+    echo $line
+    md5=$(echo $line|cut -d',' -f 1)
+    url=$(echo $line|cut -d',' -f 2)
+    if [[ $url == "" ]]; then
+        continue
+    fi
+    set +e
+    text=$(get_text_by_url $url)
+    set -e
+    if [[ $text == "" ]]; then
+        continue
+    fi
+    echo "$md5,$text" >> ./data/eval.csv
+done < ./data/urls-md5.csv
