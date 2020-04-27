043e3a57efb8b0b38440637328a2f3cacade0c5c
TAKANO Mitsuhiro <takano32@gmail.com>
2020-04-27 12:25:50 +0000

    add post-git-commit-hash.sh

---
+++ slack-bot/post-git-commit-hash.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+#
+# Setup:
+#   Set .env properly
+#
+
+#
+# Usage:
+#   cd {REPOS_ROOT}
+#   ./slack-bot/post-git-commit-hash.sh
+#
+
+set -e
+
+source .env
+
+. ./lib/slack-helper.sh
+
+channels_id=`get_channels_id ${slack_channel_develop}`
+echo -e "channels_id: $channels_id"
+
+git_commit_hash=`git rev-list --max-count=1 HEAD`
+echo -e "git_commit_hash: $git_commit_hash"
+
+
+commit_hash=$git_commit_hash
+
+echo -e "commit_hash: $commit_hash"
+commit_hash='```'
+commit_hash=$commit_hash"\r\n"
+commit_hash=$commit_hash"$git_commit_hash"
+commit_hash=$commit_hash"\r\n"
+commit_hash=$commit_hash'```'
+
+json=`cat <<EOF
+{
+    "channel": "${channels_id}",
+    "text": "現在のコミットハッシュ",
+    "blocks": [
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "現在のコミットハッシュです！"
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "${commit_hash}"
+            }
+        }
+    ]
+}
+EOF
+`
+
+echo -e "json: $json"
+wget -q -O - --post-data "$json" \
+    --header="Content-type: application/json" \
+    --header="Authorization: Bearer ${slack_token}" \
+    https://slack.com/api/chat.postMessage | jq .
+echo ""
+
+
+
+
+
