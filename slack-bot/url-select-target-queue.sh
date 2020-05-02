f7276b2adbb9a79cfbd761d8ff4be33ed69774dd
yuiseki <yuiseki@gmail.com>
2020-05-02 09:13:27 +0000

    支援制度の対象を聞く機能を追加

--- slack-bot/url-select-target-queue.sh
+++ slack-bot/url-select-target-queue.sh
@@ -4,9 +4,10 @@ set -e
 # data/reduce-vote.csv の内容を redis の queue として投入する
 # すでに投入済みの場合はスキップする
 
+. ./lib/redis-helper.sh
 . ./lib/url-helper.sh
 
-namespace="vscovid-crawler-select-type"
+namespace="vscovid-crawler-select-target"
 
 while read line; do
     # 「#」で始まる csv 行はコメントとみなしスキップする
@@ -15,9 +16,10 @@ while read line; do
     fi
 
     orgname=`echo $line| cut -d',' -f 1`
-    url=`echo $line| cut -d',' -f 2`
-    title=`echo $line| cut -d',' -f 3`
-    desc=`echo $line| cut -d',' -f 4`
+    prefname=`echo $line| cut -d',' -f 2`
+    url=`echo $line| cut -d',' -f 3`
+    title=`echo $line| cut -d',' -f 4`
+    desc=`echo $line| cut -d',' -f 5`
     md5=`get_md5_by_url $url`
 
     is_exists=`redis_exists_md5 $namespace $md5`
