5113b14260e2bb77afd332d513d271813cb16f52
kobake <pg.kobake@gmail.com>
2020-05-09 05:47:18 +0000

    env.sh のテストコード

---
+++ test/env_test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+. ./lib/test-helper.sh
+
+# .env のファイル存在チェック.
+echo "test .env: existing error case"
+set +e
+rm -f .env-test # わざと消す
+msg=`ENV_ALTERNATIVE_FILENAME=.env-test source ./lib/env.sh 2>&1`
+assert_equal "ENV ERROR: .env-test is required. See .env.sample, README, or Wiki of Repository." "$msg"
+set -e
+
+
+# .env 内パラメーター slack_channel の存在チェック.
+echo "test .env: param existing error case"
+set +e
+echo "environment=test
+slack_token=aaaa
+slack__channel=bbbb" > .env-test # slack_channel パラメーター名をわざと間違える
+msg=`ENV_ALTERNATIVE_FILENAME=.env-test source ./lib/env.sh 2>&1`
+assert_equal "ENV ERROR: .env-test parameter 'slack_channel' is required. See .env.sample, README, or Wiki of Repository." "$msg"
+set -e
+
+
+# .env 内パラメーター正常読み取りチェック
+echo "test .env: success case"
+echo "environment=test
+slack_token=aaaa
+slack_channel=bbbb" > .env-test
+msg=`ENV_ALTERNATIVE_FILENAME=.env-test source ./lib/env.sh 2>&1`
+assert_equal "" "$msg"
+
+ENV_ALTERNATIVE_FILENAME=.env-test source ./lib/env.sh
+assert_equal "test" "$environment"
+assert_equal "aaaa" "$slack_token"
+assert_equal "bbbb" "$slack_channel"
